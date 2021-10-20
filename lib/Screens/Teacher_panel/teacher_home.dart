514ac9ef6248d0da5ed818432469c87799939d6e
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-10-20 16:04:36 +0200

    Created panel for teacher

--- lib/Screens/Teacher_panel/teacher_home.dart
+++ lib/Screens/Teacher_panel/teacher_home.dart
@@ -1,22 +1,63 @@
-import 'package:edziennik/Screens/Drawer/drawer.dart';
+import 'package:edziennik/Screens/Calendar/calendar.dart';
 import 'package:edziennik/style/MyColors.dart';
 import 'package:flutter/material.dart';
 
-class TeacherHome extends StatelessWidget {
+import 'teacher_panel.dart';
+
+/*
+
+
+################################################################################################
+#######################                 NIE EDYTOWAĆ                   #########################
+################################################################################################
+
+
+
+*/
+class TeacherHome extends StatefulWidget {
+  const TeacherHome({Key? key}) : super(key: key);
+
+  @override
+  State<TeacherHome> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<TeacherHome> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    TeacherPanel(),
+    Calendar(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Teacher panel',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: MyColors.dodgerBlue,
-          title: const Text('EDziennik Nauczyciel'),
-        ),
-        drawer: MyDrawer(),
-        body: const Center(
-          child: Text(
-            'TEACHER',
-          ),
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: MyColors.dodgerBlue),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Panel nauczyciela',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Kalendarz',
+            ),
+          ],
+          selectedItemColor: MyColors.carrotOrange,
+          backgroundColor: Colors.transparent,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
       ),
     );
