c16a65d50849c38a5b7ca8a05d7032c6389bb6b7
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-10-20 16:06:25 +0200

    Added function to authorize user and redirect to right screen

--- lib/Utils/fire_auth.dart
+++ lib/Utils/fire_auth.dart
@@ -1,4 +1,10 @@
+import 'package:edziennik/Screens/Admin_panel/admin_home.dart';
+import 'package:edziennik/Screens/Student_panel/student_home.dart';
+import 'package:edziennik/Screens/Teacher_panel/teacher_home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class FireAuth {
   // For registering a new user
@@ -67,6 +73,31 @@ class FireAuth {
     User? refreshedUser = auth.currentUser;
 
     return refreshedUser;
+  }
+
+  authorizeAccess(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+
+    FirebaseFirestore.instance.collection('/users').where('uid', isEqualTo: user!.uid).get().then(
+          (docs) => {
+            if (docs.docs[0].exists)
+              {
+                if (docs.docs[0].get('role') == 'admin')
+                  {Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => AdminHome()))}
+                else if (docs.docs[0].get('role') == 'teacher')
+                  {Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => TeacherHome()))}
+                else if (docs.docs[0].get('role') == 'student')
+                  {Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => StudentHome()))}
+                else
+                  {print('invalid role')}
+              }
+            else
+              {
+                print('Account dont have a role'),
+              }
+          },
+        );
   }
 }
 
