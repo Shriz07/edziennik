81a06cbfd48387fd63dab458b713259f55728a9a
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-10-20 18:39:17 +0200

    Created panel widgets

---
+++ lib/custom_widgets/panel_widgets.dart
@@ -0,0 +1,61 @@
+import 'package:edziennik/style/MyColors.dart';
+import 'package:flutter/material.dart';
+
+Widget panelTitle(title) {
+  return Padding(
+    padding: const EdgeInsets.all(15.0),
+    child: Container(
+      width: double.infinity,
+      height: 60,
+      child: Center(
+          child: Text(
+        title,
+        style: TextStyle(fontSize: 23.0, fontWeight: FontWeight.bold, color: Colors.white),
+      )),
+      decoration: BoxDecoration(
+        color: MyColors.dodgerBlue,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget panelOption(text, action) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 70.0, right: 70.0, top: 30.0),
+    child: InkWell(
+      child: Container(
+        width: double.infinity,
+        child: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 20.0),
+          ),
+        )),
+        decoration: BoxDecoration(
+          color: MyColors.carrotOrange,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+      ),
+      onTap: action,
+    ),
+  );
+}
