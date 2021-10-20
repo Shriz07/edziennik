a9949b5b226f36cc2ca1bdda65ea3dbd8d3805cd
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-10-20 16:04:27 +0200

    Created panel for student

--- lib/Screens/Student_panel/student_home.dart
+++ lib/Screens/Student_panel/student_home.dart
@@ -1,22 +1,63 @@
-import 'package:edziennik/Screens/Drawer/drawer.dart';
+import 'package:edziennik/Screens/Calendar/calendar.dart';
 import 'package:edziennik/style/MyColors.dart';
 import 'package:flutter/material.dart';
 
-class StudentHome extends StatelessWidget {
+import 'student_panel.dart';
+
+/*
+
+
+################################################################################################
+#######################                 NIE EDYTOWAĆ                   #########################
+################################################################################################
+
+
+
+*/
+class StudentHome extends StatefulWidget {
+  const StudentHome({Key? key}) : super(key: key);
+
+  @override
+  State<StudentHome> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<StudentHome> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    StudentPanel(),
+    Calendar(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Student panel',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: MyColors.dodgerBlue,
-          title: const Text('EDziennik Uczeń'),
-        ),
-        drawer: MyDrawer(),
-        body: const Center(
-          child: Text(
-            'STUDENT',
-          ),
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: MyColors.dodgerBlue),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Panel ucznia',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Kalendarz',
+            ),
+          ],
+          selectedItemColor: MyColors.carrotOrange,
+          backgroundColor: Colors.transparent,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
       ),
     );
---
+++ lib/Screens/Student_panel/student_panel.dart
@@ -0,0 +1,24 @@
+import 'package:edziennik/Screens/Drawer/drawer.dart';
+import 'package:edziennik/style/MyColors.dart';
+import 'package:flutter/material.dart';
+
+class StudentPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Student panel',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: MyColors.dodgerBlue,
+          title: const Text('EDziennik Uczeń'),
+        ),
+        drawer: MyDrawer(),
+        body: const Center(
+          child: Text(
+            'STUDENT',
+          ),
+        ),
+      ),
+    );
+  }
+}
