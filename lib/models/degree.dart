909c0e0e342205169e4c72fbb44be65f64d3f028
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-11-25 15:05:35 +0100

    Added degree model

---
+++ lib/models/degree.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class Degree {
+  Degree({required this.userID, required this.grade, required this.date, required this.comment, required this.weight});
+
+  String userID;
+  String grade;
+  DateTime date;
+  String comment;
+  String weight;
+
+  Degree.fromMap(Map snapshot)
+      : userID = snapshot['userID'],
+        grade = snapshot['grade'],
+        date = snapshot['date'].toDate(),
+        comment = snapshot['comment'],
+        weight = snapshot['weight'];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userID': userID,
+      'grade': grade,
+      'date': Timestamp.fromDate(date),
+      'comment': comment,
+      'weight': weight,
+    };
+  }
+}
